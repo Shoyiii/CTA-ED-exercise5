acbd34fd44103133935d1998fa13a26deb0b2b98
Marion Lieutaud <72838935+MarionLieutaud@users.noreply.github.com>
2024-04-11 16:13:01 +0100

    Feedback

---
+++ Feedback.R
@@ -0,0 +1,29 @@
+# Feedback: CTA exercise 5 (2024)
+
+# question 1
+# -----------------------------------------------------------
+# very good 
+
+# question 2
+# -----------------------------------------------------------
+# excellent. Good interpretation as well. Would have been good to explain why you picked 15 topics?
+
+# question 3
+# -----------------------------------------------------------
+# you ran the code correctly but didn't interpret output. It would have been worth looking for Pretex documentation
+# to figure out what to learn from the output and what to do accordingly
+# you couldn't knit the doc because your Pretext library wasn't properly installed/loaded
+
+# overall code quality: 
+# -----------------------------------------------------------
+# excellent. You could comment a bit more. 
+# main problem was that you didn't produce a knitted html output
+# to avoid this, make sure to knit once you're done with your code, 
+# make sure all relevant libraries are included and loaded in your code
+# and finally, make sure that no important code chunk is specified as 'eval=FALSE', 
+# which tells R not to execute that code chunk
+
+# overall interpretation: 
+# -----------------------------------------------------------
+# very good work, except for question 3, where you didn't interpret the output.
+# overall, very good
